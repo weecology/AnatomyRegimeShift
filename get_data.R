577148dfe1f3cb96a032f3ab083961ef9df81de6
Morgan Ernest <morgan@weecology.org>
2025-05-28 14:15:47 -0400

    Cleaned up code repeated in functions.R

--- get_data.R
+++ get_data.R
@@ -2,18 +2,18 @@
 # Based on original code from Sarah Supp and Ellen K. Bledsoe
 
 ### LIBRARIES ### ==============================================================
-
-library(tidyverse)
 library(portalr)
 library(RCurl)
 source("functions.R")
+# library(RMark)
 
-##########################################################
+
+##################################################################
 # DATA PREP
 ##########################################################
 
 #---------------------------------------------------------
-# Clean the Data
+# Load Data
 #---------------------------------------------------------
 
 # rodent file from repo
@@ -28,25 +28,28 @@ sdat <- read.csv(text = species, header = TRUE, na.strings = c(""), stringsAsFac
 trapping <- getURL("https://raw.githubusercontent.com/weecology/PortalData/master/Rodents/Portal_rodent_trapping.csv")
 tdat <- read.csv(text = trapping, header = TRUE, stringsAsFactors = FALSE)
 
+newmoon = getURL("https://raw.githubusercontent.com/weecology/PortalData/master/Rodents/moon_dates.csv")
+mdat = read.csv(text = newmoon, header = TRUE, stringsAsFactors = FALSE)
+
+#------------------------------------------------------------------------
+# Clean data
+#-----------------------------------------------------------------------
+
+# add newmoonnumber to rodent table
+merged = rdat |> inner_join(mdat)
 # make it match Sarah Supp's data structure to use her code
-all <- repo_data_to_Supp_data(rdat, sdat) %>% 
-  filter(year <= 2015 & period > 4)
+all <- repo_data_to_Supp_data(merged, sdat) %>% 
+  filter(year <= 2015 & period > 6)
+write.csv(all, "raw_suppformat_rodents.csv") # so you don't have to keep running this bit
 
-# make table of plots and treatment types
-plots_and_treatments <- unique(all[c("plot", "plot_type")]) %>% arrange(plot)
-all_plots_all_years <- all %>% tidyr::expand(nesting(plot, plot_type), year)
 
-# Find and Remove Periods with One Day of Trapping
-
-# summarize trapping
-trap_count <- tdat %>%
-  group_by(period) %>%
-  summarise(count = sum(sampled))
-bad_periods <- filter(trap_count, count < 20) # periods that weren't fully trapped
-bad_periods <- as.list(bad_periods$period)
-
-# don't use periods with only one day of trapping
-all_no_incomplete = all[-which(all$period %in% bad_periods),] 
+#----------------------------------------------------------------
+#
+# Make Capture History time slices
+#
+#----------------------------------------------------------------
+all = read.csv("raw_suppformat_rodents.csv")
+controls_all = all |> filter(plot_type == "Control")
 
 #################################################################################
 ### Warning: the code that is commented out below might crash your computer!  ###
@@ -67,34 +70,19 @@ sp_trapping_history(all,'DS')
 sp_trapping_history(all, 'PB')
 
 
-PP_only <- filter(all, species == 'PP') |> distinct(id,period, .keep_all = TRUE)
-DM_only <- filter(all_clean, species == 'DM')  |> distinct(id,period, .keep_all = TRUE)
-DS_only <- filter(all_clean, species == 'DS')  |> distinct(id,period, .keep_all = TRUE)
-DO_only <- filter(all_clean, species == 'DO')  |> distinct(id,period, .keep_all = TRUE)
-PB_only <- filter(all_clean, species == 'PB') |> distinct(id,period, .keep_all = TRUE)
+# make table of plots and treatment types
+plots_and_treatments <- unique(all[c("plot", "plot_type")]) %>% arrange(plot)
+all_plots_all_years <- all %>% tidyr::expand(nesting(plot, plot_type), year)
 
+# Find and Remove Periods with One Day of Trapping
 
-#-----------------------------------------------------------
-# Clean repeat tags in same period
-#-----------------------------------------------------------
+# summarize trapping
+trap_count <- tdat %>%
+  group_by(period) %>%
+  summarise(count = sum(sampled))
+bad_periods <- filter(trap_count, count < 20) # periods that weren't fully trapped
+bad_periods <- as.list(bad_periods$period)
 
-# Rarely an individual moves plots during a period, resulting in
-# a tag repeated for the same period code. This code finds those reepeats
-# and selects the first capture
-
-# Create a set of capture histories by treatment and by plot if needed
-tags_all = unique(PP_only$id)
-periods_all = seq(min(PP_only$period), max(PP_only$period))
-
-#################################################################################
-### Warning: the code that is commented out below might crash your computer!  ###
-#################################################################################
-#                                                                               #
-#       The `create_trmt_hist` function to create mark_trmt_all (line 237)      #
-#       takes a long time to run. If you don't want to run it, you can          #
-#       read in the results from the GitHub repo (lines 240-1) and then         #
-#       run the RMark code -OR- skip to line 294 for all RMark results          #
-#                                                                               #
-#################################################################################
-
+# don't use periods with only one day of trapping for non-survival analyses
+all_no_incomplete = all[-which(all$period %in% bad_periods),] 
 
