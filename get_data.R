93023f5ae911fef7fcf7a48db542de32f8e04b95
Morgan Ernest <morgan@weecology.org>
2025-06-20 09:22:55 -0400

    Undo capture filters & remove unused code
    Previous capture filters were implemented because of issues calculating survival but these are better implemented elsewhere. Also removed unused code leftover from Ellen's scripts.

--- get_data.R
+++ get_data.R
@@ -9,39 +9,12 @@ source("functions.R")
 
 
 ##################################################################
-# DATA PREP
+# GET DATA 
+# Run if first time making data. Makes "raw_suppformat_rodents.csv".
+# If file already exists, skip to #Make Capture History time slices
 ##########################################################
 
-#---------------------------------------------------------
-# Load Data
-#---------------------------------------------------------
-
-# rodent file from repo
-rodents <- getURL("https://raw.githubusercontent.com/weecology/PortalData/master/Rodents/Portal_rodent.csv")
-rdat <- read.csv(text = rodents, header = TRUE, na.strings = c(""), stringsAsFactors = FALSE)
-
-# species file
-species <- getURL("https://raw.githubusercontent.com/weecology/PortalData/master/Rodents/Portal_rodent_species.csv")
-sdat <- read.csv(text = species, header = TRUE, na.strings = c(""), stringsAsFactors = FALSE)
-
-# trapping file from repo
-trapping <- getURL("https://raw.githubusercontent.com/weecology/PortalData/master/Rodents/Portal_rodent_trapping.csv")
-tdat <- read.csv(text = trapping, header = TRUE, stringsAsFactors = FALSE)
-
-newmoon = getURL("https://raw.githubusercontent.com/weecology/PortalData/master/Rodents/moon_dates.csv")
-mdat = read.csv(text = newmoon, header = TRUE, stringsAsFactors = FALSE)
-
-#------------------------------------------------------------------------
-# Clean data
-#-----------------------------------------------------------------------
-
-# add newmoonnumber to rodent table
-merged = rdat |> inner_join(mdat)
-# make it match Sarah Supp's data structure to use her code
-all <- repo_data_to_Supp_data(merged, sdat) %>% 
-  filter(year <= 2015 & period > 6)
-write.csv(all, "raw_suppformat_rodents.csv") # so you don't have to keep running this bit
-
+get_rawdata(max_year=2015, min_period = 6)
 
 #----------------------------------------------------------------
 #
@@ -51,43 +24,13 @@ write.csv(all, "raw_suppformat_rodents.csv") # so you don't have to keep running
 all = read.csv("raw_suppformat_rodents.csv")
 controls_all = all |> filter(plot_type == "Control")
 
-#################################################################################
-### Warning: the code that is commented out below might crash your computer!  ###
-#################################################################################
-#                                                                               #
-#       The `create_trmt_hist` function to create mark_trmt_all (line 237)      #
-#       takes a long time to run. If you don't want to run it, you can          #
-#       read in the results from the GitHub repo (lines 240-1) and then         #
-#       run the RMark code -OR- skip to line 294 for all RMark results          #
-#                                                                               #
-#################################################################################
+# Generate species files
+# note survivorship calculations crash < 1979 for PP, > 1994 for DS, and
+# <1994 for PB. Need to work previous data filters into survivorship code
 
-
-PP_records = all |> filter(year != 1979)
-PP_data = sp_trapping_history(PP_records, 'PP')
+PP_data = sp_trapping_history(all, 'PP')
 DM_data = sp_trapping_history(all, 'DM')
 DO_data = sp_trapping_history(all, 'DO')
-
-DS_records = all |> filter(year < 1995)
-DS_data = sp_trapping_history(DS_records,'DS')
-
-PB_records = all |> filter(year > 1994)
-PB_data = sp_trapping_history(PB_records, 'PB')
-
-
-# make table of plots and treatment types
-plots_and_treatments <- unique(all[c("plot", "plot_type")]) %>% arrange(plot)
-all_plots_all_years <- all %>% tidyr::expand(nesting(plot, plot_type), year)
-
-# Find and Remove Periods with One Day of Trapping
-
-# summarize trapping
-trap_count <- tdat %>%
-  group_by(period) %>%
-  summarise(count = sum(sampled))
-bad_periods <- filter(trap_count, count < 20) # periods that weren't fully trapped
-bad_periods <- as.list(bad_periods$period)
-
-# don't use periods with only one day of trapping for non-survival analyses
-all_no_incomplete = all[-which(all$period %in% bad_periods),] 
+DS_data = sp_trapping_history(all,'DS')
+PB_data = sp_trapping_history(all, 'PB')
 
