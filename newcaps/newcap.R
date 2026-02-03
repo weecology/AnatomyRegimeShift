05977c96ea1707b163e06d63ead9915cb2041296
Morgan Ernest <morgan@weecology.org>
2026-02-03 10:57:17 -0500

    Fix new capture calculations using unique tags
    Was counting new capture based on note= * but * was not being used reliably for new captures until mid 1990s.Used the first occurance of a unique ID to recalculate new capture data by species and newmoon

--- newcaps/newcap.R
+++ newcaps/newcap.R
@@ -1,13 +1,51 @@
-get_newcounts = function(data){
-  data = data|> mutate(time = yearmonth(paste(year,month, sep=" ")))
-  new_individuals = data |> 
-    group_by(species, time) |> 
-    filter(note2 == "*") |>  count()
-  all_individuals = data |> group_by(species,time) |> count() 
-  all_individuals = all_individuals |> 
-    full_join(new_individuals, by=join_by(time,species)) |>
-    rename(allN = n.x, newN=n.y) |> 
-    mutate(newN = replace_na(newN,0), relNew = newN/allN)
-  write.csv(all_individuals, "percent_newcaps.csv")
-  return(all_individuals)
-}
+
+#----------------------------------------------------------------
+#
+# Calculate New Individuals
+#
+#----------------------------------------------------------------
+
+# For all species on controls, calculates total number of
+# individual caught each newmoon and the number of new captures
+# new captures are determined by finding the min(time) for each
+# unique ID and then for each newmoonnumber, counting the newIDs 
+# captured in that session.
+
+library(dplyr)
+library(tsibble)
+
+# SET path
+path = "./newcaps/"
+
+# READ data and FORMAT time and replace species = NA w/ NE
+# TODO: empty species codes would also be read as NA. Need to fix
+
+data = read.csv("raw_suppformat_rodents.csv")
+data = data|> 
+  mutate(time = yearmonth(paste(year,month, sep=" ")), 
+         species = replace_na(species, "NE"))
+
+# SUBSET to control data and years of interest
+controls = data |> filter(plot_type == "Control", year > 1977 & year < 2020)
+
+# FIND first capture of each unique tag
+first_captures = controls |> 
+  group_by(id) |> 
+  filter(time == min(time)) |>
+  ungroup()
+
+# COUNT number of new captures for each newmoon and species
+new_caps = first_captures |> 
+  group_by(newmoonnumber, time, species) |> 
+  summarise(new_caps=n())
+
+# COUNT number of captures (new and recaps) by species and newmoon 
+all_captures = controls |> group_by(newmoonnumber, time, species) |> 
+  summarise(all_caps=n())
+
+# MERGE count data and CALCULATE relative number of new captures
+capture_output = all_captures |> full_join(new_caps) |> 
+  mutate(new_caps = replace_na(new_caps, 0), 
+         relative_new = new_caps/all_caps)
+write.csv(capture_output, paste(path,"percent_newcaps.csv", sep=""), 
+          row.names=FALSE)
